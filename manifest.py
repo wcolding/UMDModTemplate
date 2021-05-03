febe4b7a3f3d13e48b3527b056533fbc82964eea
Will Colding <william.colding@gmail.com>
2021-05-03 13:28:12 -0400

    Expand manifest to include subfolders and files. Added checks to ignore files that need compression.

--- manifest.py
+++ manifest.py
@@ -2,7 +2,8 @@ import os
 import hashlib
 
 srcdir = "src"
-files = os.listdir(srcdir)
+baseLevel = os.listdir(srcdir)
+baseDir = os.getcwd() + "\\" + srcdir + "\\"
 
 uncompressed = os.getcwd() + "\\UMD\\Uncompressed.umd"
 umdFile = open(uncompressed, "rb")
@@ -16,15 +17,24 @@ def GetOffset(umd, file):
 
 filelist = []
 
-for file in files:
-    if "." in file:
-        filestring = os.getcwd() + "\\" + srcdir + "\\" + file
-        curFile = open(filestring, "rb")
-        curData = curFile.read()
-        md5 = hashlib.md5(str(curData).encode('utf-8')).hexdigest()
-        filelist.append(str(file + "," + str(GetOffset(umdData, curData)) + "," + str(curFile.tell()) + "," + md5))
-        curFile.close()
+def ReadFiles(files, dir=""):
+    for file in files:
+        dirstring = baseDir + dir
+        filestring = dirstring + file
+        if "." in file:
+            # This is a file, add it to the manifest
+            curFile = open(filestring, "rb")
+            curData = curFile.read()
+            md5 = hashlib.md5(str(curData).encode('utf-8')).hexdigest()
+            filelist.append(str(dir + file + "," + str(GetOffset(umdData, curData)) + "," + str(curFile.tell()) + "," + md5))
+            curFile.close()
+        else:
+            # This is a folder, search its contents
+            nextLevel = os.listdir(filestring)
+            nextDir = dir + file + "\\"
+            ReadFiles(nextLevel, nextDir)
 
+ReadFiles(baseLevel)
 umdFile.close()
 
 manifest = open("manifest.txt", "w")
--- pack.py
+++ pack.py
@@ -7,6 +7,7 @@ manifestFile = open("manifest.txt", "r")
 manifest = csv.reader(manifestFile)
 
 changedFiles = []
+unchangedFiles = []
 
 def Xor(data):
     dataArray = bytearray(data)
@@ -16,6 +17,10 @@ def Xor(data):
         encrypted[i] = dataArray[i] ^ 183 # Key is 0xB7
     return encrypted
 
+def MatchesMD5(manifestMD5):
+    md5 = hashlib.md5(str(fdata).encode('utf-8')).hexdigest()
+    return md5 == manifestMD5
+
 
 for file in manifest:
     filepath = dir + "\\src\\" + file[0]
@@ -23,21 +28,44 @@ for file in manifest:
     try:
         f = open(filepath, "rb")
         fdata = f.read()
-        if f.tell() == int(file[2]):
-            print("Passed size check (" + file[2] + " bytes)")
-            md5 = hashlib.md5(str(fdata).encode('utf-8')).hexdigest()
-            if md5 in file[3]:
-                print("Passed md5 check. No change in file.\n")
+
+        if int(file[1]) > -1:
+            
+            if f.tell() == int(file[2]):
+                print("Passed size check (" + file[2] + " bytes)")
+                
+                if MatchesMD5(file[3]):
+                    print("Passed md5 check. No change in file.\n")
+                else:
+                    changedFiles.append(file)
+                    
             else:
-                changedFiles.append(file)
+                print("File failed size check and cannot be packed.\n")
+                unchangedFiles.append(file)  
+
+        else:
+            
+            if MatchesMD5(file[3]):
+                print("File requires compression and is unchanged. Skipping.\n")
+            else:
+                print("File requires compression and cannot be packed.\n")
+                unchangedFiles.append(file)  
+
         f.close()
+
     except:
         print(file[0] + " was unable to be opened")
 
 manifestFile.close()
 
+if (len(unchangedFiles) > 0):
+    print("The following files could not be packed:")
+    for file in unchangedFiles:
+        print("- " + file[0])
+    print("")
+
 numFiles = len(changedFiles)
-print(str(numFiles) + " file(s) will be changed")
+print(str(numFiles) + " file(s) will be changed:")
 
 if (numFiles > 0):
     try:
@@ -56,12 +84,11 @@ if (numFiles > 0):
        filepath = dir + "\\src\\" + file[0]
        f = open(filepath, "rb")
        fdata = f.read()
-       #patchedBackup = bytes(patchedData)
        fileEndOffset = int(file[1]) + int(file[2])
        patchedData = patchedData[0:int(file[1])] + fdata + patchedData[fileEndOffset:unpackedSize]
        f.close()
 
-    print("Encoding data...")
+    print("\nEncoding data...")
     encryptedData = Xor(patchedData)
 
     patchedPath = dir + "\\Build\\Conviction.umd"
